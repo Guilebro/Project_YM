bd45e128ee685ef35ed260f556fae7a0b3f4aa66
Maxime Dupin <maximedupin@live.fr>
2023-02-16 12:13:35 +0100

    avant retour sur front

--- backend/model/gameModel.js
+++ backend/model/gameModel.js
@@ -3,7 +3,7 @@ const connexion = require("../config");
 const db = connexion.promise();
 
 const findAllGames = () => {
-  return db.query("SELECT * FROM games");
+  return db.query("SELECT * FROM game");
 };
 
 module.exports = { findAllGames };
--- frontend/src/components/FilterLeft.jsx
+++ frontend/src/components/FilterLeft.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "../css/FilterLeft.css";
 
 function FilterLeft() {
   return (
-    <div>
+    <div className="main_filterLeft">
       <p>FilterLeft</p>
     </div>
   );
