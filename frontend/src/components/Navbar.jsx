24517bcb45997c1d532a3c8ac643ead7e81c5bbb
Maxime Dupin <maximedupin@live.fr>
2023-02-16 18:23:13 +0100

    ajout logo + css général

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/bulleform3.png";
+import facebook from "../assets/logos/facebook.png";
+import instagram from "../assets/logos/instagram.png";
+import twitter from "../assets/logos/twitter.png";
+import french from "../assets/logos/french.png";
+import uk from "../assets/logos/uk.png";
+
 import "../css/Navbar.css";
 
 function Navbar() {
@@ -11,9 +17,18 @@ function Navbar() {
       </Link>
       <div className="searchBar_contact">
         <div className="contact">
-          <span>Contacte-moi</span>
-          <span>Logo Reseaux</span>
-          <span>Switch langues</span>
+          <a href="mailto:yanngrard@gmail.com" target="_blank" rel="noreferrer">
+            <span className="contact_link">Contactez-nous</span>
+          </a>
+          <span className="contact_logo">
+            <img className="logo_sn" src={facebook} alt="facebook" />
+            <img className="logo_sn" src={instagram} alt="instagram" />
+            <img className="logo_sn" src={twitter} alt="twitter" />
+          </span>
+          <span className="flag_logo">
+            <img className="flag" src={french} alt="french" />
+            <img className="flag" src={uk} alt="uk" />
+          </span>
         </div>
         <input
           type="search"
