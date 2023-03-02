d3f95ebf90bbe43064d79ca2f1b40e46fc08ca05
Maxime Dupin <maximedupin@live.fr>
2023-03-02 14:46:05 +0100

    wip css et mise en place des selects dans filerLeft

--- frontend/src/components/FilterLeft.jsx
+++ frontend/src/components/FilterLeft.jsx
@@ -1,10 +1,63 @@
 import React from "react";
 import "../css/FilterLeft.css";
+import {
+  ageOption,
+  durationOption,
+  languageOption,
+  editorOption,
+  playerOption,
+} from "../utils/allOption";
 
 function FilterLeft() {
   return (
     <div className="main_filterLeft">
-      <p>FilterLeft</p>
+      <div className="doudi">
+        <div className="age_select">
+          <label htmlFor="age">Age</label>
+          <select id="">
+            <option value=""> Tous </option>
+            {ageOption.map((age) => (
+              <option value="">{age.option}</option>
+            ))}
+          </select>
+        </div>
+        <div className="duration_select">
+          <label htmlFor="duration">Durée</label>
+          <select id="">
+            <option value=""> Toute </option>
+            {durationOption.map((duration) => (
+              <option value="">{duration.option}</option>
+            ))}
+          </select>
+        </div>
+        <div className="editor_select">
+          <label htmlFor="editor">Editeur</label>
+          <select id="">
+            <option value=""> Toute </option>
+            {editorOption.map((editor) => (
+              <option value="">{editor.option}</option>
+            ))}
+          </select>
+        </div>
+        <div className="language_select">
+          <label htmlFor="language">Langue</label>
+          <select id="">
+            <option value=""> Toute </option>
+            {languageOption.map((language) => (
+              <option value="">{language.option}</option>
+            ))}
+          </select>
+        </div>
+        <div className="player_select">
+          <label htmlFor="player">Nb de joueurs</label>
+          <select id="">
+            <option value=""> Tous </option>
+            {playerOption.map((player) => (
+              <option value="">{player.option}</option>
+            ))}
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
